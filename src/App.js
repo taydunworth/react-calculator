b3e82f27994c1ea8caafc92d5d26da114d990968
Taylor Cox <taylorashtoncox@gmail.com>
2019-02-19 18:44:19 -0500

    Creates layout of calculator and adds numbers to input box (no math yet)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
+import { ClearButton } from './components/ClearButton';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ""
+    }
+  }
+
+  addToInput = val => {
+    this.setState({input: this.state.input + val});
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <div className="calculator-wrapper">
+          <Input input={this.state.input}></Input>
+          <div className="row">
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>x</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>-</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>
+          </div>
+          <div className="row">
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>=</Button>
+          </div>
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({input: ""})}>Clear</ClearButton>
+          </div>
+        </div>
       </div>
     );
   }
